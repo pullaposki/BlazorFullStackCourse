4429daef38946e4c98aabb7ad4d670b2721118d4
pullaposki <pullaposki@windowslive.com>
2023-11-13 10:31:40 +0200

    List app started

---
+++ DataModels/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorApp1.DataModels
+{
+	public class Product
+	{
+		public string Id { get; set; }
+		public int AskingPrice { get; set; }	
+		public int Units { get; set; }
+		public int Total => Units * AskingPrice; // Can't be SET from outside
+	}
+}
